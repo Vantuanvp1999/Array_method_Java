0e2df484b54c34da540423f515e795efef4c8409
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-09 11:16:40 +0700

    addbtend

---
+++ src/numberOfChar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class numberOfChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str = sc.nextLine();
+        System.out.println("Enter the character:");
+        char ch = sc.next().charAt(0);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (ch == str.charAt(i)) {
+                count++;
+            }
+        }
+        System.out.println("The result is: "+count);
+    }
+}
